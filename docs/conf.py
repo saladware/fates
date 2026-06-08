96eaa574fe55ee813df638c097d470fd458d6a8f
Evgeny Goryachev <saladware46@gmail.com>
2026-06-08 13:05:30 +0300

    chore: bump version to 0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "fates"
 copyright = "2026, Evgeny Goryachev"  # noqa: A001
 author = "Evgeny Goryachev"
-release = "0.0.1"
+release = "0.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
